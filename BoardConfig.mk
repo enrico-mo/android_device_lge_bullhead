3725a73def3a8f40355eb82c6b4b2b0bc53ed0fd
enrico-mo <enrico.caporali.mo@gmail.com>
2018-08-26 22:36:42 +0200

    Added TWRP flags to BoardConfig + twrp.fstab (from TeamWin repo)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,3 +166,16 @@ BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS := hardware/cyanogen/cmhw
 
 -include vendor/lge/bullhead/BoardConfigVendor.mk
+
+# TWRP
+RECOVERY_VARIANT := twrp
+PRODUCT_COPY_FILES += \
+    device/lge/bullhead/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
