2f0ea5a91bcdd61fc07eb7f40c01cc5ff9feda15
enrico-mo <enrico.caporali.mo@gmail.com>
2019-04-28 11:08:38 +0200

    Added properties for franco kernel on BoardConfig (alternative flags los/franco kernel sources)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,13 @@ TARGET_NO_BOOTLOADER := true
 
 # Inline kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := lineageos_bullhead_defconfig
+# LOS Kernel
+#TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+#TARGET_KERNEL_CONFIG := lineageos_bullhead_defconfig
+# Franco Kernel
+TARGET_KERNEL_SOURCE := kernel/franco/bullhead
+TARGET_KERNEL_CONFIG := franco_defconfig
+
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
